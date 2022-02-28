a67edf23aa91a6109ecc47878e895c1b38b7813e
Sean Biller <seanbiller@USA-M1P-SB.lan>
2022-02-28 11:48:49 -0600

    vanillaJS Webpack example with proper webpack.config.js for css loading

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import _ from "lodash";
+import algoliasearch from "algoliasearch/lite";
+import instantsearch from "instantsearch.js";
+import { searchBox, hits } from "instantsearch.js/es/widgets";
+
+import "instantsearch.css/themes/satellite.css";
+
+const searchClient = algoliasearch(
+  "FKOQ4JOZSE",
+  "4233f3625bbc1f27a5c7b3859a6e1a24"
+);
+
+const search = instantsearch({
+  indexName: "WorkRamp Index",
+  searchClient,
+});
+
+search.addWidgets([
+  searchBox({
+    container: "#searchbox",
+  }),
+
+  hits({
+    container: "#hits",
+  }),
+]);
+
+search.start();
